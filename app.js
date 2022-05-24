b5b1bf60a9f3a6607e03544fcd62fddb994684fb
Cipher <techvisualworld@gmail.com>
2022-05-24 23:14:54 +0530

    api changed

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.get("/", function(req, res) {
   res.sendFile(__dirname + "/signup.html");
 });
 
-app.post("/failure",function(req,res){
+app.post("/failure", function(req, res) {
   res.redirect("/");
 })
 
@@ -40,14 +40,14 @@ app.post("/", function(req, res) {
 
   const options = {
     method: "POST",
-    auth: "cipher:0242032787ded713e9a28fb31a80c517-us18",
+    auth: "cipher:a35f955061be7f760abbcc8ecd3684f4-us18",
   }
 
   const request = https.request(url, options, function(response) {
-    if(response.statusCode === 200){
+    if (response.statusCode === 200) {
       res.sendFile(__dirname + "/success.html");
-    }else{
-      res.sendFile(__dirname+"/failure.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
     }
     response.on("data", function(data) {
       console.log(JSON.parse(data));
@@ -61,7 +61,7 @@ app.post("/", function(req, res) {
 
 
 
-app.listen(process.env.PORT||3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server is up!!");
 });
 
